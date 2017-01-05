915f6ddee1b1b464207479b9a4ad6cde19cfd0f9
STEPHANE50 <stephane.levasseur@pole-emploi.fr>
2017-01-05 16:03:32 +0100

    Commit sur correction bug

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -3,19 +3,22 @@ package co.simplon.factorielle;
 /**
  * Factorielle
  */
-public class Factorielle 
-{
-    public Factorielle() {
-    		super();
-    }
-    
-    public long calculer(long n) {
-    		if (n > 1) {
-    			return n * calculer(n-1);
-    		} else if (n < 0) {
-    			throw new IllegalArgumentException();
-    		}
-    		return n;
-    }
+public class Factorielle {
+	public Factorielle() {
+		super();
+	}
+
+	public long calculer(long n) {
+		if (n > 1) {
+			return n * calculer(n - 1);
+		} else if (n == 0) {
+			return n = 1;
+
+		} else if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+
+		return n;
+	}
 
 }
--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -4,11 +4,74 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 public class FactorielleTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCalculerFactoriellede0() {
+		// Param GIVEN
+		long entier = 0;
+		Factorielle factorielle = new Factorielle();
+
+		// Resultat attendu WHEN
+
+		long resultat = factorielle.calculer(entier);
+
+		// Le resultat doit être 1 THEN
+
+		assertEquals(1, resultat);
+	}
+	
+	@Test
+	public void testCalculerFactoriellede1() {
+		// Param GIVEN
+		long entier = 1;
+		Factorielle factorielle = new Factorielle();
+
+		// Resultat attendu WHEN
+
+		long resultat = factorielle.calculer(entier);
+
+		// Le resultat doit être 1 THEN
+
+		assertEquals(1, resultat);
 	}
 
+	@Test
+	public void testCalculerFactoriellede15() {
+		// GIVEN Paramètres
+		long entier = 19;
+		Factorielle factorielle = new Factorielle();
+
+		// Resultat attendu WHEN
+
+		long resultat = factorielle.calculer(entier);
+
+		// THEN Le resultat doit être factorielle de 25 THEN
+
+		assertEquals(121645100408832000l, resultat);
+	}
+	/*/@Test
+	public void testCalculerFactoriellede20() {
+		// GIVEN Paramètres
+		bignumber entier = 19;
+		Factorielle factorielle = new Factorielle();
+
+		// Resultat attendu WHEN
+
+		long resultat = factorielle.calculer(entier);
+
+		// THEN Le resultat doit être factorielle de 25 THEN
+
+		assertEquals(121645100408832000l, resultat);
+	}
+
+	@Test
+	public void testCalculerException() throws Exception {
+
+		Fail ( IllegalArgumentException("le nombre doit être superieur à 0."));
+	}
+
+	*/
 }
